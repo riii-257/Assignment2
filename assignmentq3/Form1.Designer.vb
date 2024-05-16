fcfe33bbc7d41d8a3753cf6afde7b38d3bb9e7b6
riya.inmca2126 <riya.inmca2126@OPENSOURCELAB08>
2024-05-16 09:44:01 +0530

    Add project files.

---
+++ assignmentq3/Form1.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Label1 = New Label()
+        Label2 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Button1 = New Button()
+        ErrorProvider1 = New ErrorProvider(components)
+        ErrorProvider2 = New ErrorProvider(components)
+        CType(ErrorProvider1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(ErrorProvider2, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(90, 62)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(41, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Label1"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(90, 123)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(41, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Label2"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(193, 59)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 2
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(193, 120)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 3
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(193, 193)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 4
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' ErrorProvider1
+        ' 
+        ErrorProvider1.ContainerControl = Me
+        ' 
+        ' ErrorProvider2
+        ' 
+        ErrorProvider2.ContainerControl = Me
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(499, 294)
+        Controls.Add(Button1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(ErrorProvider1, ComponentModel.ISupportInitialize).EndInit()
+        CType(ErrorProvider2, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ErrorProvider1 As ErrorProvider
+    Friend WithEvents ErrorProvider2 As ErrorProvider
+
+End Class
---
+++ assignmentq3/Form1.vb
@@ -0,0 +1,41 @@
+﻿Imports System.Text.RegularExpressions
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.Text = "Mobile No"
+        Label2.Text = "Email ID"
+        Button1.Text = " Validate"
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ValidateMobileNumber()
+        ValidateEmailAddress()
+    End Sub
+    Private Sub ValidateMobileNumber()
+        Dim mobileNumber As String = TextBox1.Text.Trim()
+
+        If String.IsNullOrEmpty(mobileNumber) Then
+            ErrorProvider1.SetError(TextBox1, "Mobile number is required.")
+        ElseIf Not Regex.IsMatch(mobileNumber, "^\d{10}$") Then
+            ErrorProvider1.SetError(TextBox1, "Invalid mobile number format. Please enter 10 digits.")
+        Else
+            ErrorProvider1.SetError(TextBox1, "")
+            MsgBox("Valid Phone Number")
+        End If
+    End Sub
+
+    Private Sub ValidateEmailAddress()
+        Dim email As String = TextBox2.Text.Trim()
+
+        If String.IsNullOrEmpty(email) Then
+            ErrorProvider1.SetError(TextBox2, "Email address is required.")
+        ElseIf Not Regex.IsMatch(email, "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$") Then
+            ErrorProvider1.SetError(TextBox2, "Invalid email address format.")
+        Else
+            ErrorProvider1.SetError(TextBox2, "")
+            MsgBox("Valid E-mail ID")
+        End If
+    End Sub
+
+End Class
